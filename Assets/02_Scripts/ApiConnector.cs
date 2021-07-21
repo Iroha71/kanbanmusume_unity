e209148cd53ae21ea472083c69896a480ddebecc
Kobayashi-Yuto <kobayashi.works@fujitsu.com>
2021-07-21 09:12:55 +0900

    [update] ヘッダーに認証トークンを付与 #4

--- Assets/02_Scripts/ApiConnector.cs
+++ Assets/02_Scripts/ApiConnector.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 public class ApiConnector : MonoBehaviour
 {
     private const string BASE_URL = "http://127.0.0.1:5500";
+    private static string userAuthToken = "";
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +19,22 @@ public class ApiConnector : MonoBehaviour
         
     }
 
+    public static string ReadAuthToken()
+    {
+        if (userAuthToken.Equals(""))
+        {
+            string json = Resources.Load<TextAsset>("save").ToString();
+            SaveData savedata = JsonUtility.FromJson<SaveData>(json);
+            userAuthToken = savedata.token;
+        }
+
+        return userAuthToken;
+    }
     public static async UniTask<string> Get(string endpoint)
     {
         UnityWebRequest request = UnityWebRequest.Get(BASE_URL + endpoint);
+        print(BASE_URL + endpoint);
+        request.SetRequestHeader("Authorization", $"Bearer { ReadAuthToken() }");
         await request.SendWebRequest();
 
         if (request.result == UnityWebRequest.Result.ConnectionError)
